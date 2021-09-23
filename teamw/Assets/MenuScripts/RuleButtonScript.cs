03bbb611714508510115441af8439a256c35bbe5
Futagami <60266261.futagami.cgp@gmail.com>
2021-09-23 22:26:01 +0900

    202109232225
    クレジットの廃止
    ルール画面の作成

--- teamw/Assets/MenuScripts/RuleButtonScript.cs
+++ teamw/Assets/MenuScripts/RuleButtonScript.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CreditButtonScript : MonoBehaviour
+public class RuleButtonScript : MonoBehaviour
 {
     //ボタンが押されたときに呼び出される関数
     public void Select()
     {
         //行われる操作 クレジットへ移動
-        SceneManager.LoadScene("Credit");
+        SceneManager.LoadScene("Rule");
     }
 }
